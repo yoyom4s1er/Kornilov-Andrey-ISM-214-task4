c62d629216f9d0a455778c847e6973d95dc30d13
yoyom4s1er <80907085+yoyom4s1er@users.noreply.github.com>
2021-10-13 22:24:32 +0300

    Update vehicle.cpp

--- vehicle.cpp
+++ vehicle.cpp
@@ -2,7 +2,11 @@
 #include <iostream>
 
 vehicle::vehicle(const double speed, const std::string mark, const int pas)
-	: speed(speed), mark(mark), pas(pas) {}
+{
+	this->speed = speed;
+	this->mark = mark;
+	this->pas = pas;
+}
 
 double vehicle::get_speed() const {
 	return this->speed;
@@ -16,21 +20,48 @@ int vehicle::get_pas() const {
 	return this->pas;
 }
 
-void vehicle::set_speed(const double speed) {
-	if (speed >= 0) {
+void vehicle::set_speed(const double& speed) {
+	try
+	{
+		if (speed < 0)
+		{
+			throw "The speed cannot be negative";
+		}
 		this->speed = speed;
 	}
-}
-
-void vehicle::set_mark(const std::string mark) {
-	if (mark != "") {
-		this->mark = mark;
+	catch (const char* exception)
+	{
+		std::cerr << "Error: " << exception << '\n';
 	}
 }
 
-void vehicle::set_pas(const int pas) {
-	if (pas > 0) {
+void vehicle::set_mark(const std::string& mark) {
+	try
+	{
+		if (mark == "")
+		{
+			throw "the name cannot be empty";
+		}
+		this->mark = mark;
+	}
+	catch (const char* exception)
+	{
+		std::cerr << "Error: " << exception << '\n';
+	}
+}
+
+void vehicle::set_pas(const int& pas) {
+	try
+	{
+		if (pas < 0)
+		{
+			throw "The pas cannot be negative";
+		}
 		this->pas = pas;
+	}
+	catch (const char* exception)
+	{
+		std::cerr << "Error: " << exception << '\n';
 	}
 }
 
@@ -52,12 +83,29 @@ double car::get_fuel_rate(const double distance) {
 	return this->ratio * distance;
 }
 
-void car::set_ratio(const double ratio) {
-	this->ratio = ratio;
+void car::set_ratio(const double& ratio) {
+	try
+	{
+		if (ratio < 0)
+		{
+			throw "The ratio cannot be negative";
+		}
+		this->ratio = ratio;
+	}
+	catch (const char* exception)
+	{
+		std::cerr << "Error: " << exception << '\n';
+	}
 }
 
 lorry::lorry(const double speed, const std::string mark, const int pas, const double ratio, const double load_capacity)
-	: vehicle(speed, mark, pas), ratio(ratio), load_capacity(load_capacity) {}
+{
+	this->speed = speed;
+	this->mark = mark;
+	this->pas = pas;
+	this->ratio = ratio;
+	this->load_capacity = load_capacity;
+}
 
 double lorry::get_ratio() const {
 	return this->ratio;
@@ -72,10 +120,32 @@ double lorry::get_fuel_rate(const double distance) {
 }
 
 
-void lorry::set_ratio(const double ratio) {
-	this->ratio = ratio;
+void lorry::set_ratio(const double& ratio) {
+	try
+	{
+		if (ratio < 0)
+		{
+			throw "The ratio cannot be negative";
+		}
+		this->ratio = ratio;
+	}
+	catch (const char* exception)
+	{
+		std::cerr << "Error: " << exception << '\n';
+	}
 }
 
-void lorry::set_load_capacity(const double load_capacity) {
-	this->load_capacity = load_capacity;
+void lorry::set_load_capacity(const double& load_capacity) {
+	try
+	{
+		if (ratio < 0)
+		{
+			throw "The load capacity cannot be negative";
+		}
+		this->load_capacity = load_capacity;
+	}
+	catch (const char* exception)
+	{
+		std::cerr << "Error: " << exception << '\n';
+	}
 }
